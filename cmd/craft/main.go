9f75c925974b33c00c0b6d732c92983073efab7f
charconstpointer <attwistingnether@gmail.com>
2020-12-29 06:37:54 +0100

    server

--- cmd/craft/main.go
+++ cmd/craft/main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/charconstpointer/glowstone/pkg/glowstone"
 )
 
+var (
+	addr = flag.String("addr", ":9000", "address to listen on for http requests")
+)
+
 func main() {
+	flag.Parse()
 	s := glowstone.NewServer()
-	s.Listen(":9999")
+	s.Listen(*addr)
 }
--- pkg/glowstone/server.go
+++ pkg/glowstone/server.go
@@ -2,7 +2,10 @@ package glowstone
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
@@ -11,50 +14,85 @@ import (
 	"github.com/go-chi/chi/middleware"
 )
 
-type Server interface {
+type GlowstoneServer interface {
 	Listen(addr string) error
 	CreateServer() error
 }
 
-type GlowServer struct {
+type HTTPServer struct {
+	servers []*Server
 }
 
-func NewServer() *GlowServer {
-	return &GlowServer{}
+type Server struct {
+	ID        string `json:"id"`
+	createdAt int    `json:"createdAt"`
 }
 
-func (s *GlowServer) CreateServer() error {
+func NewServer() *HTTPServer {
+	return &HTTPServer{
+		servers: make([]*Server, 0),
+	}
+}
+
+func (s *HTTPServer) addServers(serv *Server) error {
+	for _, sv := range s.servers {
+		if sv.ID == serv.ID {
+			fmt.Errorf("server %s already exists on glowstone server", serv.ID)
+		}
+	}
+	s.servers = append(s.servers, serv)
+	return nil
+}
+
+func (s *HTTPServer) CreateServer() error {
 	ctx := context.Background()
 	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
-		panic(err)
+		return err
 	}
 	resp, err := cli.ContainerCreate(ctx, &container.Config{
 		Image: "itzg/minecraft-server",
 		Env:   []string{"EULA=TRUE"},
 	}, nil, nil, nil, "")
 	if err != nil {
-		panic(err)
+		return err
 	}
-
+	serv := &Server{
+		ID:        resp.ID,
+		createdAt: int(time.Now().Unix()),
+	}
+	s.addServers(serv)
 	if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
-		panic(err)
+		return err
 	}
 	return err
 }
 
-func (s *GlowServer) Listen(addr string) error {
+func (s *HTTPServer) Listen(addr string) error {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Post("/servers", func(rw http.ResponseWriter, r *http.Request) {
-		err := s.CreateServer()
-		if err != nil {
-			rw.WriteHeader(http.StatusInternalServerError)
-			rw.Write([]byte("could not create new server"))
-			return
-		}
-		rw.WriteHeader(http.StatusAccepted)
-	})
+	r.Post("/servers", s.handleCreateServer)
+	r.Get("/servers", s.handleGetServers)
 	err := http.ListenAndServe(addr, r)
 	return err
+}
+
+func (s *HTTPServer) handleCreateServer(rw http.ResponseWriter, r *http.Request) {
+	go s.CreateServer()
+	rw.WriteHeader(http.StatusAccepted)
+}
+
+func (s *HTTPServer) handleGetServers(rw http.ResponseWriter, r *http.Request) {
+	servers := s.servers
+	if len(servers) == 0 {
+		rw.WriteHeader(http.StatusNotFound)
+		return
+	}
+	rw.WriteHeader(http.StatusOK)
+	b, err := json.Marshal(servers)
+	if err != nil {
+		rw.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	rw.Write(b)
 }
