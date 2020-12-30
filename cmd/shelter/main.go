a20d8361b46238749e45e2096e372b60fba3ff67
charconstpointer <attwistingnether@gmail.com>
2020-12-30 11:30:19 +0100

    tunnel idea

--- cmd/shelter/main.go
+++ cmd/shelter/main.go
@@ -1,23 +1,67 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"net"
+	"time"
 )
 
 func main() {
-	l, err := net.Listen("tcp", ":8888")
+	conn, err := net.Dial("tcp", ":8889")
 	if err != nil {
 		log.Fatal(err.Error())
 	}
-	for {
-		conn, err := l.Accept()
-		if err != nil {
-			log.Fatal(err.Error())
-		}
+	server := ":25565"
+	ds, err := net.Dial("tcp", server)
+	log.Println(ds.LocalAddr())
+	if err != nil {
+		log.Fatal(err.Error())
 
-		go func(c net.Conn) {
-			log.Println("handle", c.RemoteAddr())
-		}(conn)
+	}
+	log.Println("connected to downstream server", ds.RemoteAddr())
+	go handleClientStream(ds, conn)
+	go handleServerStream(conn, ds)
+	time.Sleep(time.Second * 999999)
+}
+func ignore() {
+	// l, err := net.Listen("tcp", ":8888")
+	// if err != nil {
+	// 	log.Fatal(err.Error())
+	// }
+	// for {
+	// 	conn, err := l.Accept()
+	// 	if err != nil {
+	// 		log.Fatal(err.Error())
+	// 	}
+
+	// 	go func(c net.Conn) {
+	// 		server := ":25565"
+	// 		log.Println("handle new client", c.RemoteAddr(), "connecting to server", server)
+	// 		ds, err := net.Dial("tcp", server)
+	// 		log.Println(ds.LocalAddr())
+	// 		if err != nil {
+	// 			log.Fatal(err.Error())
+
+	// 		}
+	// 		log.Println("connected to downstream server", ds.RemoteAddr())
+	// 		go handleClientStream(ds, c)
+	// 		go handleServerStream(c, ds)
+
+	// 	}(conn)
+	// }
+}
+func handleServerStream(c io.Writer, ds io.Reader) {
+	if _, err := io.Copy(c, ds); err != nil {
+		log.Println("stream closed")
+		fmt.Println(err)
+	}
+}
+
+func handleClientStream(ds io.Writer, c io.Reader) {
+	if _, err := io.Copy(ds, c); err != nil {
+		log.Println("stream closed")
+		fmt.Println(err)
 	}
 }
