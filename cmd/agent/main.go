918a7d21b447ae5a3e3b4c4cd6b7dd3a7840376a
charconstpointer <attwistingnether@gmail.com>
2021-01-04 06:10:50 +0100

    igave up

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"flag"
 	"log"
+	"net"
 
 	"github.com/charconstpointer/glowstone/pkg/glowstone"
+	"github.com/golang/protobuf/proto"
 )
 
 var (
@@ -14,13 +16,59 @@ var (
 
 func main() {
 	flag.Parse()
-	agent, err := glowstone.NewAgent(*tunnel, *mc)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
+	conn, _ := net.Dial("tcp", ":8889")
+	downstream, _ := net.Dial("tpc", ":25565")
+	go func(upstream net.Conn) {
+		b := make([]byte, 32*1024)
+		for {
+			n, err := upstream.Read(b)
 
-	err = agent.Listen()
-	if err != nil {
-		log.Fatal(err.Error())
-	}
+			if err != nil {
+				log.Fatal(err.Error())
+				continue
+			}
+			if n > 0 {
+				var tick glowstone.Tick
+				err := proto.Unmarshal(b[:n], &tick)
+				if err != nil {
+					log.Fatal(err.Error())
+				}
+
+				n, err := downstream.Write(tick.Payload)
+				if err != nil {
+					log.Fatal(err.Error())
+				}
+				log.Printf("wrote %d bytes to minecraft", n)
+			}
+		}
+	}(conn)
+
+	go func(dest string, downstream net.Conn, upstream net.Conn) {
+		b := make([]byte, 32*1024)
+		for {
+			n, err := downstream.Read(b)
+
+			if err != nil {
+				log.Fatal(err.Error())
+				continue
+			}
+			if n > 0 {
+				tick := glowstone.Tick{
+					Src:     "mc",
+					Dest:    dest,
+					Payload: b[:n],
+				}
+				b, err := proto.Marshal(&tick)
+				if err != nil {
+					log.Fatal(err.Error())
+				}
+
+				n, err := upstream.Write(b)
+				if err != nil {
+					log.Fatal(err.Error())
+				}
+				log.Printf("wrote %d bytes upstream %s", n, upstream.RemoteAddr().String())
+			}
+		}
+	}(conn.RemoteAddr().String(), downstream, conn)
 }
--- cmd/tunnel/main.go
+++ cmd/tunnel/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"flag"
 	"log"
+	"net"
 
 	"github.com/charconstpointer/glowstone/pkg/glowstone"
+	"google.golang.org/protobuf/proto"
 )
 
 var (
@@ -14,9 +16,64 @@ var (
 
 func main() {
 	flag.Parse()
-	tunnel := glowstone.NewTunnel(*up, *down)
-	err := tunnel.Listen()
+	dc, err := net.Listen("tcp", *down)
 	if err != nil {
 		log.Fatal(err.Error())
+	}
+	ds, err := dc.Accept()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	uc, err := net.Listen("tcp", *up)
+	us, err := uc.Accept()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	b := make([]byte, 32*1024)
+	go func() {
+		for {
+
+			n, err := us.Read(b)
+			if err != nil {
+				log.Fatal(err.Error())
+			}
+			if n > 0 {
+				tick := glowstone.Tick{
+					Src:     us.RemoteAddr().String(),
+					Dest:    "mc",
+					Payload: b[:n],
+				}
+				msg, err := proto.Marshal(&tick)
+				if err != nil {
+					log.Fatal(err.Error())
+				}
+				n, err = ds.Write(msg)
+				log.Println(n)
+			}
+		}
+	}()
+
+	for {
+
+		n, err := us.Read(b)
+
+		if err != nil {
+			log.Fatal(err.Error())
+			continue
+		}
+		if n > 0 {
+			var tick glowstone.Tick
+			err := proto.Unmarshal(b[:n], &tick)
+			if err != nil {
+				log.Fatal(err.Error())
+			}
+
+			n, err := ds.Write(tick.Payload)
+			if err != nil {
+				log.Fatal(err.Error())
+			}
+			log.Printf("wrote %d bytes to minecraft", n)
+		}
 	}
 }
