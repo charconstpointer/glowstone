bf3441d579efb82c8f2bf78383a98125e257d364
charconstpointer <attwistingnether@gmail.com>
2021-02-02 00:43:58 +0100

    buffer size

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -8,8 +8,8 @@ import (
 
 func main() {
 	m := glowstone.NewMux()
-	if err := m.Dial("ec2-3-8-238-16.eu-west-2.compute.amazonaws.com:8000"); err != nil {
-		// if err := m.Dial(":8000"); err != nil {
+	// if err := m.Dial("ec2-3-8-238-16.eu-west-2.compute.amazonaws.com:8000"); err != nil {
+	if err := m.Dial(":8000"); err != nil {
 		// if err := m.Dial("159.89.4.159:8000"); err != nil {
 		log.Println(err.Error())
 	}
